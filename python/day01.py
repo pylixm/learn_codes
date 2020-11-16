88eaa9248ecf5c133773de783e86fbcf110ed744
xiumin1 <pyli.xm@gmail.com>
2020-11-16 23:11:08 +0800

    day01.py

---
+++ python/day01.py
@@ -0,0 +1,31 @@
+"""
+给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution(object):
+
+    def remove_duplicates(self, nums: list) -> int:
+        if not nums:
+            return 0
+
+        point = 0
+        for i in range(len(nums) - 1):
+            if nums[point] != nums[i + 1] and nums[point] == nums[i]:
+                # 交换
+                nums[point + 1] = nums[i + 1]
+                point += 1
+
+        return point + 1
+
+
+if __name__ == '__main__':
+    num_list = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    solution = Solution()
+    print(solution.remove_duplicates(num_list))
